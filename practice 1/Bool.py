86ab29ed90af6533909c4d4a933307dab3f3e636
EunSe-o <dkan601@gmail.com>
2020-08-15 02:53:53 +0900

    파이썬 기초1

---
+++ practice 1/Bool.py
@@ -0,0 +1,9 @@
+# 참 / 거짓
+
+print(5>10) # False
+print(5<10) # True
+print(True)
+print(False)
+print(not True) # False
+print(not False) # True
+print(not(5>10)) # True
---
+++ practice 1/print.py
@@ -0,0 +1,14 @@
+print(5)
+print(3.14)
+print(1000)
+
+print(5+3)
+print(5*3)
+print(6/2)
+print(6%4)
+print(3*(3+1))
+
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋ")
+print("ㅋ"*5)
